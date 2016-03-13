f604dee21012fb566eb2bb6910538643a033fc82
Erik Trinh <trinh.erik@gmail.com>
2016-03-12 22:39:38 -0500

    attemp fix regexp match #2

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,9 +4,9 @@ function textResponse(response, postData) {
     console.log("request handler textResponse called");
     response.writeHead(200, {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"});
     if(/^send me help fam/i.test(postData.Body)) {
-        var arr = postData.Body.match(/-?\d*.*\d*/g);
+        var arr = postData.Body.match(/-?\d+\.?\d*/g);
         console.log(arr);
-        var arr2 = Array.prototype.map.call(arr, parseFloat);
+        var arr2 = arr.map(parseFloat);
         console.log(arr2);
         mapsStuff.getDirs({lat: arr2[0], lng: arr2[1]}, {lat: arr2[2], lng: arr2[3]}, response);
     }
