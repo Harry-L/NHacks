d33d280007017cdbe0726367e2af4c6072f1e0e9
Erik Trinh <trinh.erik@gmail.com>
2016-03-13 03:26:41 -0400

    update dependencies

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,16 +1,24 @@
 var mapsStuff = require('./mapsStuff');
+var twilioComp = require('./twilioComp');
 
 function textResponse(response, postData) {
     console.log("request handler textResponse called");
-    response.writeHead(200, {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"});
     if(/^send me home fam/i.test(postData.Body)) {
+        response.writeHead(200, {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"});
         var arr = postData.Body.match(/-?\d+\.?\d*/g);
         console.log(arr);
         var arr2 = arr.map(parseFloat);
         console.log(arr2);
         mapsStuff.getDirs({lat: arr2[0], lng: arr2[1]}, {lat: arr2[2], lng: arr2[3]}, response);
     }
+
+    else if(/^find my fam/i.test(postData.Body)) { // user sends text , find my fam 'phonenumber'
+        var target = postData.Body.match(/+\d{10}/)[0];
+        twilioComp.(target, 'Go to the find my fam app to verify that ' + postData.From + ' is your fam!');
+        response.end();
+    }
     else {
+        response.writeHead(200, {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"});
         var directionsString = "No handler for request, to: " + postData.To + " From: " + postData.From + " body: " + postData.Body;
         response.write(directionsString);
         response.end();
